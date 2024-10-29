0dfa6d42dac68710f6d270beb194a4a4f5213dc9
Debasis_sahu <sahurocky524@gmail.com>
2024-10-29 11:00:27 +0530

    correct the changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
 // import Test from './components/pages/prac/Test';
-// import Account from './components/pages/userAuth/UserAuth';
+import Account from './components/pages/userAuth/UserAuth';
 
 const App = () => {
   return (
     <>
       {/* <Test /> */}
-      {/* <Account /> */}
-      <h1>hello</h1>
-      <h2>dub</h2>
+      <Account />
     </>
   )
 }
